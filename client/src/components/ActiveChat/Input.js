c52d44e9d7ebd006a785b7d00946948b24daa32c
vincenttse15 <vincenttse15@gmail.com>
2022-01-15 19:11:55 -0800

    upload to cloudinary

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,42 +1,82 @@
 import React, { useState } from "react";
-import { FormControl, FilledInput } from "@material-ui/core";
+import { FormControl, FilledInput, IconButton, useTheme } from "@material-ui/core";
+import FileCopyOutlinedIcon from '@mui/icons-material/FileCopyOutlined';
 import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import { postMessage } from "../../store/utils/thunkCreators";
+import axios from "axios";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     justifySelf: "flex-end",
-    marginTop: 15
+    marginTop: 15,
+    position: 'relative',
   },
   input: {
     height: 70,
     backgroundColor: "#F4F6FA",
     borderRadius: 8,
-    marginBottom: 20
-  }
+    marginBottom: 20,
+    paddingRight: theme.spacing(7),
+  },
+  uploadBtn: {
+    position: 'absolute',
+    right: '1%',
+    top: '13%',
+  },
 }));
 
 const Input = (props) => {
-  const classes = useStyles();
+  const theme = useTheme();
+  const classes = useStyles(theme);
   const [text, setText] = useState("");
+  const [files, setFiles] = useState([]);
+  const [isUploading, setIsUploading] = useState(false);
   const { postMessage, otherUser, conversationId, user } = props;
 
-  const handleChange = (event) => {
+  const handleTextChange = (event) => {
     setText(event.target.value);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const urlArray = [];
+
+    if (files.length > 0) {
+      setIsUploading(true);
+      await uploadToCloudinary(urlArray);
+      setIsUploading(false);
+    }
     // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
     const reqBody = {
       text: event.target.text.value,
       recipientId: otherUser.id,
       conversationId,
-      sender: conversationId ? null : user
+      sender: conversationId ? null : user,
+      attachments: urlArray.length > 0 ? urlArray : null,
     };
     await postMessage(reqBody);
     setText("");
+  };
+
+  const handleFileChange = (event) => {
+    setFiles(event.target.files);
+  }
+
+  const uploadToCloudinary = async (urlArray) => {
+    for (const file of files) {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("upload_preset", "hatchways");
+
+      try {
+        const { data } = await axios.post("https://api.cloudinary.com/v1_1/drlylnzt8/image/upload", formData);
+        urlArray.push(data.secure_url);
+      } catch (error) {
+        console.log(error);
+        return;
+      }
+    }
   };
 
   return (
@@ -48,9 +88,21 @@ const Input = (props) => {
           placeholder="Type something..."
           value={text}
           name="text"
-          onChange={handleChange}
+          onChange={handleTextChange}
+          disabled={isUploading}
         />
       </FormControl>
+      <IconButton color="secondary" className={classes.uploadBtn} component="label">
+        <FileCopyOutlinedIcon />
+        <input
+          type="file"
+          hidden
+          accept="image/*"
+          multiple
+          onChange={handleFileChange}
+          disabled={isUploading}
+        />
+      </IconButton>
     </form>
   );
 };
--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -8,7 +8,9 @@ import {
 } from "../conversations";
 import { gotUser, setFetchingStatus } from "../user";
 
-axios.interceptors.request.use(async function (config) {
+const axiosInstance = axios.create();
+
+axiosInstance.interceptors.request.use(async function (config) {
   const token = await localStorage.getItem("messenger-token");
   config.headers["x-access-token"] = token;
 
@@ -20,7 +22,7 @@ axios.interceptors.request.use(async function (config) {
 export const fetchUser = () => async (dispatch) => {
   dispatch(setFetchingStatus(true));
   try {
-    const { data } = await axios.get("/auth/user");
+    const { data } = await axiosInstance.get("/auth/user");
     dispatch(gotUser(data));
     if (data.id) {
       socket.emit("go-online", data.id);
@@ -34,7 +36,7 @@ export const fetchUser = () => async (dispatch) => {
 
 export const register = (credentials) => async (dispatch) => {
   try {
-    const { data } = await axios.post("/auth/register", credentials);
+    const { data } = await axiosInstance.post("/auth/register", credentials);
     await localStorage.setItem("messenger-token", data.token);
     dispatch(gotUser(data));
     socket.emit("go-online", data.id);
@@ -46,7 +48,7 @@ export const register = (credentials) => async (dispatch) => {
 
 export const login = (credentials) => async (dispatch) => {
   try {
-    const { data } = await axios.post("/auth/login", credentials);
+    const { data } = await axiosInstance.post("/auth/login", credentials);
     await localStorage.setItem("messenger-token", data.token);
     dispatch(gotUser(data));
     socket.emit("go-online", data.id);
@@ -58,7 +60,7 @@ export const login = (credentials) => async (dispatch) => {
 
 export const logout = (id) => async (dispatch) => {
   try {
-    await axios.delete("/auth/logout");
+    await axiosInstance.delete("/auth/logout");
     await localStorage.removeItem("messenger-token");
     dispatch(gotUser({}));
     socket.emit("logout", id);
@@ -71,7 +73,7 @@ export const logout = (id) => async (dispatch) => {
 
 export const fetchConversations = () => async (dispatch) => {
   try {
-    const { data } = await axios.get("/api/conversations");
+    const { data } = await axiosInstance.get("/api/conversations");
     dispatch(gotConversations(data));
   } catch (error) {
     console.error(error);
@@ -79,7 +81,7 @@ export const fetchConversations = () => async (dispatch) => {
 };
 
 const saveMessage = async (body) => {
-  const { data } = await axios.post("/api/messages", body);
+  const { data } = await axiosInstance.post("/api/messages", body);
   return data;
 };
 
@@ -111,7 +113,7 @@ export const postMessage = (body) => (dispatch) => {
 
 export const searchUsers = (searchTerm) => async (dispatch) => {
   try {
-    const { data } = await axios.get(`/api/users/${searchTerm}`);
+    const { data } = await axiosInstance.get(`/api/users/${searchTerm}`);
     dispatch(setSearchedUsers(data));
   } catch (error) {
     console.error(error);
