cc6c51b489dcabfc88e89e9f266bba0deaba86ee
vincenttse15 <vincenttse15@gmail.com>
2022-01-17 17:52:36 -0800

    remove env variable for now

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL, {
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://postgres:root@localhost:5432/messenger", {
   logging: false
 });
 
